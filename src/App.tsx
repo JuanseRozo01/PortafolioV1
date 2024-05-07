b368f5c428a6c92cf932bb978b7a4d4f5305ea76
Juan sebastian rozo <famlrozo@hotmail.com>
2024-05-07 17:24:59 -0500

    implementacion de lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,76 @@
-import { AboutMe } from './Components/AboutMe/AboutMe'
-import { Contact } from './Components/Contact/Contact'
-import { Home } from './Components/Home/Home'
-import { Navbar } from './Components/Navbar/Navbar'
-import { ParticlesComponent } from './Controllers/Particles'
-import { Skills } from './Components/Skills/Skills'
-import { Projects } from './Components/Projects/Projects'
+import { Suspense, lazy, ComponentType } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Footer } from './Components/Footer/Footer'
+
+const LazyNavbar = lazy(() => import('./Components/Navbar/Navbar').then(module => ({default: module.Navbar as ComponentType })));
+const LazyHome = lazy(() => import('./Components/Home/Home').then(module => ({ default: module.Home as ComponentType })));
+const LazyAboutMe = lazy(() => import('./Components/AboutMe/AboutMe').then(module => ({ default: module.AboutMe as ComponentType })));
+const LazyParticlesComponent = lazy(() => import('./Controllers/Particles').then(module => ({ default: module.ParticlesComponent as ComponentType })));
+const LazySkills = lazy(() => import('./Components/Skills/Skills').then(module => ({ default: module.Skills as ComponentType})));
+const LazyProjects = lazy(() => import('./Components/Projects/Projects').then(module => ({ default: module.Projects as ComponentType})));
+const LazyContact = lazy(() => import('./Components/Contact/Contact').then(module => ({ default: module.Contact as ComponentType})));
+const LazyFooter = lazy(() => import('./Components/Footer/Footer').then(module => ({default: module.Footer as ComponentType})));
 
 function App() {
-
-
-  return (
-    <BrowserRouter>
+    return (
+      <BrowserRouter>
       <div>
       
       <div id='home'>
+      <Suspense fallback={<h1 style={{color: "white"}}>loading...</h1>}>
       <Routes>
-        <Route path='/' element={<Navbar/>}/>  
+        <Route path='/' element={<LazyNavbar/>}/> 
       </Routes>
+        </Suspense> 
       </div>
 
       <div id='homepage'>
+      <Suspense fallback={<h1>loading...</h1>}>
       <Routes>
-        <Route path='/' element={<Home/>}/>    
+        <Route path='/' element={<LazyHome/>}/>
       </Routes>
+        </Suspense>    
       </div>
 
       <div id='aboutme'>
+      <Suspense fallback={<h1>loading...</h1>}>
       <Routes>
-        <Route path='/' element={<AboutMe/>}/>   
+        <Route path='/' element={<LazyAboutMe/>}/>
       </Routes>
+      </Suspense>   
       </div>
 
       <div id='skills'>
+      <Suspense fallback={<h1>loading...</h1>}>
       <Routes>
-        <Route path='/' element={<Skills/> }/>    
+        <Route path='/' element={<LazySkills/> }/>
       </Routes>
+      </Suspense>    
       </div>
 
       <div id='projects'>
+      <Suspense fallback={<h1>loading...</h1>}>
       <Routes>
-        <Route path='/' element={<Projects/>}/>    
+        <Route path='/' element={<LazyProjects/>}/>  
       </Routes>
+      </Suspense>  
       </div> 
         
       <div id='contact'>
+      <Suspense fallback={<h1>loading...</h1>}>
       <Routes>
-        <Route path='/' element={<Contact/>}/>    
+        <Route path='/' element={<LazyContact/>}/>
       </Routes>
+      </Suspense>    
       </div>
-      <ParticlesComponent/>
 
-      <Footer/>
+      <Suspense fallback={<h1>loading...</h1>}>
+      <LazyParticlesComponent/>
+      <LazyFooter/>
+      </Suspense>
       </div>
       
     </BrowserRouter>
+      
   )
 }
 
